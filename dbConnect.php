a896b323bdfc3edadc55bf7d16706166ae15a45c
hughallen <hugh.allen95@gmail.com>
2017-01-04 19:43:42 +0000

    Added dbConnect

---
+++ dbConnect.php
@@ -0,0 +1,16 @@
+<? php
+
+$db_host = "localhost";
+$db_username = "root";
+$db_password = "root";
+$db_name = "HCV";
+
+//connecting to the database:
+
+$db_connect = mysqli_connect($db_host, $db_username, $db_password, $db_name) or die ("Unable to connect to MySQL");
+
+//check valid connection:
+
+echo "Connected to MySQL<br>";
+
+?>
